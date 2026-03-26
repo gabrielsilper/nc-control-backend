7335186ca65b528d9b1033b8ce3472f7b4a8c564
gabrielsilper <gabrielsilper.main@gmail.com>
2026-03-26 11:10:30 -0400

    feat: adiciona jwt e suas configs, bcrypt e pequenos ajustes em userservice

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,45 +1,60 @@
-import { Repository } from 'typeorm';
-import { AppDataSource } from '../config/database';
+import UserCreateDTO from 'dtos/user-create.dto';
+import { EmailAlreadyExistsError } from 'errors/email-already-exists.error';
+import { UserNotFoundError } from 'errors/user-not-found.error';
+import IEncrypterService from 'interfaces/encrypter-service';
+import UserRepository from 'repositories/user.repository';
 import User from '../entities/users';
-import bcrypt from 'bcrypt';
 
 export default class UserService {
-  private userRepository: Repository<User>;
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly encrypter: IEncrypterService,
+  ) {}
 
-  constructor() {
-    this.userRepository = AppDataSource.getRepository(User);
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  async findAll(): Promise<User[]> {
-    return await this.userRepository.find();
+  async findById(id: string): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+
+    return user;
   }
 
-  async findById(id: string): Promise<User | null> {
-    return await this.userRepository.findOne({ where: { id } });
-  }
+  async create(userData: UserCreateDTO): Promise<User> {
+    const userExists = await this.userRepository.findOneBy({
+      email: userData.email,
+    });
 
-  async create(userData: { name: string; email: string; password: string; profile: string }): Promise<User> {
-    const existingUser = await this.userRepository.findOne({ where: { email: userData.email } });
-    if (existingUser) throw new Error('Email já cadastrado');
-    
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
-    const user = this.userRepository.create({ ...userData, password: hashedPassword });
-    return await this.userRepository.save(user);
+    if (userExists) {
+      throw new EmailAlreadyExistsError();
+    }
+
+    const password = await this.encrypter.encrypt(userData.password);
+
+    const user = this.userRepository.create({ ...userData, password });
+
+    return this.userRepository.save(user);
   }
 
   async update(id: string, userData: Partial<User>): Promise<User> {
     const user = await this.findById(id);
-    if (!user) throw new Error('Usuário não encontrado');
-    
-    if (userData.password) userData.password = await bcrypt.hash(userData.password, 10);
-    
-    Object.assign(user, userData);
-    return await this.userRepository.save(user);
+
+    if (userData.password) {
+      userData.password = await this.encrypter.encrypt(userData.password);
+    }
+
+    const userUpdated = this.userRepository.merge(user, userData);
+
+    return await this.userRepository.save(userUpdated);
   }
 
   async delete(id: string): Promise<void> {
     const user = await this.findById(id);
-    if (!user) throw new Error('Usuário não encontrado');
-    await this.userRepository.delete(id);
+    await this.userRepository.delete(user);
   }
 }
