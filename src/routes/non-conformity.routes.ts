31c0f98486f738f7a54c416b3a4cd8f7561e5186
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-02 23:20:45 -0400

    refactor: ajustas as rotas de auth e de non-conformities

--- src/routes/non-conformity.routes.ts
+++ src/routes/non-conformity.routes.ts
@@ -22,24 +22,24 @@ const validateTokenMiddleware = new ValidateTokenMiddleware(new TokenService());
 const nonConformityRoutes = Router();
 
 nonConformityRoutes.post(
-  '/non-conformity',
+  '/',
   (req, res, next) => validateTokenMiddleware.handle(req as RequestWithPayload, res, next),
   validateBody(createNonConformitySchema),
   (req, res) => nonConformityController.create(req as RequestWithPayload, res),
 );
-nonConformityRoutes.get('/non-conformity', (req, res) => res.end(req.url));
-nonConformityRoutes.get('/non-conformity/:id', (req, res) => res.end(req.url));
-nonConformityRoutes.put('/non-conformity', (req, res) => res.end(req.url));
-nonConformityRoutes.delete('/non-conformity', (req, res) => res.end(req.url));
+nonConformityRoutes.get('/', (req, res) => res.end(req.url));
+nonConformityRoutes.get('/:id', (req, res) => res.end(req.url));
+nonConformityRoutes.put('/', (req, res) => res.end(req.url));
+nonConformityRoutes.delete('/', (req, res) => res.end(req.url));
 
 // Essa vai ser para atualizar o responsável
-nonConformityRoutes.patch('/non-conformity/:id/assigne/:user', (req, res) => res.end(req.url));
+nonConformityRoutes.patch('/:id/assigne/:user', (req, res) => res.end(req.url));
 
 // Essa vai ser para atualizar a data de expiração
-nonConformityRoutes.patch('/non-conformity/:id/due-date/:date', (req, res) => res.end(req.url));
+nonConformityRoutes.patch('/:id/due-date/:date', (req, res) => res.end(req.url));
 
 // Essa vai ser para finalizar a não conformidade
-nonConformityRoutes.patch('/non-conformity/:id/fisish', (req, res) => res.end(req.url));
+nonConformityRoutes.patch('/:id/fisish', (req, res) => res.end(req.url));
 
 // Falta imaginar para atualizar os status
 
