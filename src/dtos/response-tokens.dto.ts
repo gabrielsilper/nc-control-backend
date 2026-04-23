8e3c7d7b13cc7029129ca717621e264a60ea7d9a
gabrielsilper <gabrielsilper.main@gmail.com>
2026-04-23 11:26:29 -0400

    feat: agora o login e refresh retornam o id e profile do usuário

--- src/dtos/response-tokens.dto.ts
+++ src/dtos/response-tokens.dto.ts
@@ -1,4 +1,12 @@
+import { Profile } from 'enums/profile.enum';
+
+export interface AuthUserDTO {
+  id: string;
+  profile: Profile;
+}
+
 export default interface ResponseTokensDTO {
   accessToken: string;
   refreshToken: string;
+  user: AuthUserDTO;
 }
